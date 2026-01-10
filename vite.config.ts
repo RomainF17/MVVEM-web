76af5638dc3816bdc0fdb5c28cd695b319780837
RomainF17 <romain.fargier@protonmail.com>
2026-01-10 19:24:49 +0100

    Force cache busting via filenames

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Force new filenames every build to bust cache
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].${Date.now()}.js`,
+        chunkFileNames: `assets/[name].${Date.now()}.js`,
+        assetFileNames: `assets/[name].${Date.now()}.[ext]`
+      }
+    }
+  }
 })
